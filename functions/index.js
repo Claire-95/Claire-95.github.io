da02168592c89634b755ad302474af96b9c58a5d
Claire Reed <clairejr95@gmail.com>
2022-06-26 18:03:42 +0100

    deploy function 1

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,23 @@
 /* eslint-disable object-curly-spacing */
 const functions = require("firebase-functions");
+const express = require("express");
+const bodyParser = require("body-parse");
+const admin = require("firebase-admin");
 
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
+admin.initializeApp(functions.config().firebase);
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", { structuredData: true });
-  response.send("Hello from Firebase!");
-});
+// initialize express server
+const app = express();
+const main = express();
+
+// add the path to receive request and set json as bodyParser to process body
+main.use("/api/v1", app);
+main.use(bodyParser.json());
+main.use(bodyParser.urlencoded({ extended: false }));
+
+// initialize the database and the collection
+// const db = admin.firestore();
+// const userCollection = "users";
+
+// define google cloud function name
+exports.webApi = functions.https.onRequest(main);
