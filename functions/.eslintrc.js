436490bb7e479c0f2b0cb641b7e2314adf1644b7
Claire Reed <clairejr95@gmail.com>
2022-06-27 18:53:00 +0100

    eslint functions ecma 6

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -1,14 +1,15 @@
 /* eslint-disable quote-props */
 module.exports = {
   root: true,
-  ecmaVersion: 6,
   env: {
     es6: true,
     node: true,
   },
+  parserOptions: {
+    ecmaVersion: 8,
+  },
   extends: ["eslint:recommended", "google"],
   rules: {
     quotes: ["error", "double"],
-    "arrow-body-style": ["warn"],
   },
 };
