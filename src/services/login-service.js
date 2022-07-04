6db9b1c4ddad02158c4cc8066b0413cd843a8fae
Claire Reed <clairejr95@gmail.com>
2022-07-04 18:27:31 +0100

    locally persistent login

--- src/services/login-service.js
+++ src/services/login-service.js
@@ -1,15 +1,20 @@
 exports.HandleLogin = (loginState) => {
   if (loginState) {
     console.log("Congratulations you're logged in!");
+    localStorage.setItem("login", "true");
   } else {
-    console.log("You're not logged in...");
+    console.log("You're signed out...");
+    localStorage.removeItem("login");
   }
   console.log(loginState);
   exports.loggedIn = loginState;
   updateLoginStateCallback(exports.loggedIn);
 };
-
 exports.loggedIn = false;
+const persistentLogin = localStorage.getItem("login");
+if (persistentLogin !== null) {
+  exports.loggedIn = true;
+}
 
 exports.UpdateLoginState = (updateLoginStateCallbackValue) => {
   updateLoginStateCallback = updateLoginStateCallbackValue;
