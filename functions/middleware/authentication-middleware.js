8ac16016e60fa408aaf47f5e3274f3e4695e6350
claire.reed <claire.j.reed@hotmail.co.uk>
2022-08-04 00:16:34 +0100

    Handle errors in authentication middleware, store id in user and log

--- functions/middleware/authentication-middleware.js
+++ functions/middleware/authentication-middleware.js
@@ -1,15 +1,24 @@
+/* eslint-disable indent */
 const admin = require("firebase-admin");
 
 module.exports = (req, res, next) => {
-  req.user = {};
-  const authHeader = req.get("Authentication");
-  if (
-    authHeader !== undefined &&
-    admin.auth().verifyIdToken(authHeader.replace("Bearer ", ""))
-  ) {
-    req.user.loggedIn = true;
-  } else {
+  try {
+    req.user = {};
+    const authHeader = req.get("Authentication");
+    if (authHeader !== undefined) {
+      admin
+        .auth()
+        .verifyIdToken(authHeader.replace("Bearer ", ""))
+        .then((res) => {
+          req.user.id = res.uid;
+          req.user.email = res.email;
+          req.user.loggedIn = true;
+          console.log(`Authenticated user: ${JSON.stringify(req.user)}`);
+          next();
+        });
+    }
+  } finally {
     req.user.loggedIn = false;
+    next();
   }
-  next();
 };
