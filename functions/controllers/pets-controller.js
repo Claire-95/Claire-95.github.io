88f9ca5351d7fe57d1f6d1495181ff866f4803bf
claire.reed <claire.j.reed@hotmail.co.uk>
2022-08-04 00:53:32 +0100

    log

--- functions/controllers/pets-controller.js
+++ functions/controllers/pets-controller.js
@@ -33,9 +33,9 @@ router.get("/", async (req, res) => {
 // Post a Pet to database
 router.post("/", async (req, res) => {
   try {
-    const data = JSON.parse(req.body);
-    data.owner = req.user.email;
-    await db.collection(petCollection).doc().set(data);
+    console.log(req.user);
+    req.body.owner = req.user.email;
+    await db.collection(petCollection).doc().set(req.body);
     res.status(200).json({});
   } catch (error) {
     console.log(error);
