6c83d9723120d6a62524f9957d7691d88e665c86
claire.reed <claire.j.reed@hotmail.co.uk>
2022-08-11 21:14:44 +0100

    Update AllPets.jsx

--- src/pages/AllPets.jsx
+++ src/pages/AllPets.jsx
@@ -1,7 +1,9 @@
 import { GetPets } from "../services/pet-service";
+import { GetCounter } from "../services/counter-service";
 import LoginService from "../services/login-service";
 
 const AllPets = () => {
+  GetCounter();
   console.log(LoginService);
   if (LoginService.loggedIn) {
     return GetPets();
