ed3074565fffcc0de327c70bd485e78827db3996
claire.reed <claire.j.reed@hotmail.co.uk>
2022-08-23 19:28:24 +0100

    Update pets-controller.js

--- functions/controllers/pets-controller.js
+++ functions/controllers/pets-controller.js
@@ -34,8 +34,6 @@ router.get("/", async (req, res) => {
 router.post("/", async (req, res) => {
   try {
     req.body.owner = req.user.email;
-
-    req.body.owner = req.user.email;
     await db.collection(petCollection).doc().set(req.body);
     res.status(200).json({});
   } catch (error) {
