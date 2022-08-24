a0d7d279137c242a7eacbefd99900c6e51585419
claire.reed <claire.j.reed@hotmail.co.uk>
2022-08-24 12:20:44 +0100

    More updates, think I got it.
    Added the new owner to array before sending to pet-service.

--- src/pages/EditPet.jsx
+++ src/pages/EditPet.jsx
@@ -4,11 +4,13 @@ import { UpdatePet } from "../services/pet-service";
 const EditPet = () => {
   function EditPetHandler(updatedPetData) {
     console.log(updatedPetData);
+    const newOwner = updatedPetData.sharedOwners;
 
     var newData = {
       id: updatedPetData.id,
       name: updatedPetData.name,
       species: updatedPetData.species,
+      sharedOwners: [{ newOwner }],
     };
     console.log(newData);
 
