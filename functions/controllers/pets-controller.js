bd0a08d0dc80e6284359eb145643d0148259383e
claire.reed <claire.j.reed@hotmail.co.uk>
2022-08-25 15:54:26 +0100

    Duh, forgot the query

--- functions/controllers/pets-controller.js
+++ functions/controllers/pets-controller.js
@@ -29,7 +29,7 @@ router.get("/", async (req, res) => {
 
     const secondPetQuerySnapshot = await db
       .collection(petCollection)
-      .where("owner", "==", req.user.email)
+      .where("sharedOwners", "in", [req.user.email])
       .get();
     const sharedPets = [];
     secondPetQuerySnapshot.forEach((doc) => {
