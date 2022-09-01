47e55daf65a87d53bf4c9d399d59eb104716a362
claire.reed <claire.j.reed@hotmail.co.uk>
2022-09-01 09:59:04 +0100

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -40,4 +40,7 @@ exports.webApi = functions.https.onRequest(main);
 
 exports.scheduledFunction = functions.pubsub
   .schedule("every 5 minutes")
-  .onRun(app);
+  .onRun((context) => {
+    console.log("made it to schedule");
+    return app;
+  });
