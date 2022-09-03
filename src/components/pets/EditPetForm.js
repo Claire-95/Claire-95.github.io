1066c929bed72e0267069e9d96f6e2a0b1bce9a7
claire.reed <claire.j.reed@hotmail.co.uk>
2022-09-03 12:14:49 +0100

    Update EditPetForm.js

--- src/components/pets/EditPetForm.js
+++ src/components/pets/EditPetForm.js
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import { useRef } from "react";
 import Card from "../ui/Card";
 import classes from "./NewPetForm.module.css";
 import PetsIcon from "@mui/icons-material/Pets";
