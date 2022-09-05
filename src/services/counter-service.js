5c3f40d5dffe627da22572281ec56beea77aa216
claire.reed <claire.j.reed@hotmail.co.uk>
2022-09-05 11:25:47 +0100

    Just removed GetCounters as no longer used

--- src/services/counter-service.js
+++ src/services/counter-service.js
@@ -1,60 +1,7 @@
 import restClient from "./rest-client-service";
-import { useState, useEffect } from "react";
-import { CounterList } from "../components/pets/CounterList";
 
 const urlBase = require("../constants");
 const axios = require("axios").default;
-
-const GetCounters = () => {
-  const [isLoading, setIsLoading] = useState(true);
-  const [loadedCounters, setLoadedCounters] = useState([]);
-
-  useEffect(() => {
-    setIsLoading(true);
-    restClient()
-      .get("counters")
-      .then((response) => {
-        return response.data;
-      })
-      .then((data) => {
-        const counters = [];
-
-        for (const key in data) {
-          const counter = {
-            id: key,
-            ...data[key],
-          };
-
-          counters.push(counter);
-        }
-        setIsLoading(false);
-        setLoadedCounters(counters);
-      });
-  }, []);
-
-  if (isLoading) {
-    return (
-      <section>
-        <p>Loading...</p>
-      </section>
-    );
-  }
-
-  const urlId = window.location.pathname.split("/").pop();
-  console.log(urlId);
-  const counters = [];
-  console.log(loadedCounters);
-
-  console.log(loadedCounters[0].data.linkedPet);
-
-  for (var i = 0; i < loadedCounters.length; i++) {
-    if (loadedCounters[i].data.linkedPet === urlId) {
-      counters.push(loadedCounters[i]);
-    }
-  }
-  console.log(counters);
-  return <CounterList counters={counters} />;
-};
 
 const SetCounter = (props) => {
   var counterData = props;
@@ -93,4 +40,4 @@ const DeleteCounter = (counterId) => {
 };
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export { GetCounters, DeleteCounter, SetCounter, UpdateCounter };
+export { DeleteCounter, SetCounter, UpdateCounter };
